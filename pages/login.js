7ceaaa18255ed7e371645b315e652cbc8ea424d3
Hendra juice <hd.brandoz@gmail.com>
2018-10-07 20:10:26 +0700

    create page signup

--- pages/login.js
+++ pages/login.js
@@ -43,31 +43,31 @@ class Login extends Component {
     return (
       <div className="container mt-4">
         <Head>
-          <title key="headTitle">Login - CJ Apps</title>
+          <title key="headTitle">Login - CJ Property</title>
         </Head>
         <div className="row justify-content-center">
           <div className="col-lg-4 col-md-6 col-sm-8">
             <div className="card">
               <div className="card-body">
                 <h3 className="card-title">Login</h3>
-                <form method="POST" onSubmit={this.submitLogin}>
+                <form method="POST" onSubmit={this.submitLogin} noValidate>
                   <div className="form-group">
                     <label>Email</label>
-                    <input type="email" className="form-control" value={this.state.email} onChange={this.handleChangeEmail}/>
+                    <input type="email" className="form-control" value={this.state.email} onChange={this.handleChangeEmail} />
                   </div>
                   <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" value={this.state.password} onChange={this.handleChangePassword}/>
+                    <input type="password" className="form-control" value={this.state.password} onChange={this.handleChangePassword} />
                   </div>
                   <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input" checked={this.state.remember} onChange={this.handleChangeRemember}/>
+                    <input type="checkbox" className="form-check-input" checked={this.state.remember} onChange={this.handleChangeRemember} />
                     <label className="form-check-label"> Remember me</label>
                   </div>
                   <button className="btn btn-primary" type="submit">Login</button>
                 </form>
               </div>
               <div className="card-footer text-muted text-center">
-                Not registered yet? <Link href="#"><a>Sign up</a></Link>
+                Not registered yet? <Link href="/signup"><a>Sign up</a></Link>
               </div>
             </div>
             <div className="text-center py-2">
